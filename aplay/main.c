339d216e5047b5460150b08552c391ee3d7efe13
cbrpnk <emmanuelturpin@gmail.com>
2023-05-02 00:34:38 -0400

    optimizing tiny synth

--- aplay/main.c
+++ aplay/main.c
@@ -1,37 +1,26 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <math.h>
 
 #define BUFFER_SIZE 4096
 #define CHANNEL_COUNT 2
 
-#define DELAY_LINE_SIZE 44100
-
 #define MIN(a, b) (a) < (b) ? (a) : (b)
 #define MAX(a, b) (a) > (b) ? (a) : (b)
 
-float delay_buf[DELAY_LINE_SIZE] = { 0 };
-int delay_ptr = 0;
-
-void delay_write(float v) {
-    delay_buf[delay_ptr++] = v;
-    delay_ptr %= DELAY_LINE_SIZE;
-}
-
-float delay_read(int i) {
-    int pos = 0;
-    if(delay_ptr < i) {
-        pos = (i - delay_ptr);
-        pos = DELAY_LINE_SIZE - pos - 1;
-    } else {
-        pos = delay_ptr - i;
+float m_exp(float a) {
+    //return pow(M_E, a);
+    float floor_a = (int) a;
+    float fract_a = a - floor_a;
+    float res = M_E;
+    for(int i=0; i<floor_a-1; ++i) {
+        res *= M_E;
     }
-    return delay_buf[delay_ptr];
+    return res * (1 + fract_a*(1+(fract_a/2)*(1 + (fract_a/3) * (1 + fract_a/4))));
 }
 
 float env(float time, float release) {
-    return 1.0f/pow(2.0f, time*release);
+    return 1.0f/m_exp(time*release);
 }
 
 /*
@@ -68,13 +57,14 @@ float modulo(float a, float modulus) {
     return a;
 }
 
-
 float tan_h(float a) {
-    return (2.0f / (1 + (1/pow(M_E, 2*a)))) - 1;
+    return (2.0f / (1 + (1/m_exp(2*a)))) - 1;
 }
 
+int seed = 1;
 float noise() {
-    return (float) rand() / RAND_MAX;
+    int res = (seed = (48271*seed) % 2147483647);
+    return (float) res / (4294967295U);
 }
 
 float drum(float time) {
@@ -86,14 +76,14 @@ float drum(float time) {
 
 float last = 0.;
 float lead(float time) {
-    float f = 30 * (2 * (int)(modulo(time*2, 3))) * (int)(modulo(time*4+(int)(m_sin(time)), 4));
+    float f = 30 * (2 * (int)((time*2, 3))) * (int)(modulo(time*4+(int)(m_sin(time)), 4));
     time = modulo(time, .5);
     float mod3 = osc(time, f*120, last*.5) * .2;
     float mod2 = tan_h(osc(time, f*16, mod3)) * 3;
     float mod1 = osc(time, f*2, mod2) * 10;
     
-    float pitch_env = 1.0f/pow(2.0f, time*10.0f);
-    last = tanh(osc(time, 60, env(time, 10) + mod1) * env(time, 20) * 100.);
+    float pitch_env = env(time, 10.0f);
+    last = tan_h(osc(time, 60, env(time, 10) + mod1) * env(time, 20) * 100.);
     return last;
 }
 
@@ -110,9 +100,11 @@ void play(float time, float *left, float *right) {
     float kick = drum(modulo(time, .5));
     
     float l = lead(time) * .25;
+    /*
     float d = delay_read(2);
     delay_write(l);
     l = -.5 *l + .2 * d;
+    */
     
     float clap = do_clap(time);
     float hihat = do_hihat(time);
@@ -130,13 +122,21 @@ void _start () {
     asm volatile("sub $8, %rsp\n");
     int frame_id = 0;
     float buffer[BUFFER_SIZE*CHANNEL_COUNT];
+    float *buf = &buffer[0];
     while(1) {
         for(int i=0; i<BUFFER_SIZE*CHANNEL_COUNT; i+=2) {
             float time = (float) frame_id / 44100;
             play(time, buffer+i, buffer+i+1);
             frame_id++;
         }
-        write(1, &buffer, sizeof(float) * BUFFER_SIZE*CHANNEL_COUNT);
+        asm volatile (
+            "mov $1, %%rax;"
+            "mov $1, %%rdi;"
+            "mov %0, %%rsi;"
+            "mov %1, %%rdx;"
+            "syscall;"
+            :: "m"(buf), "i"(sizeof(float) * BUFFER_SIZE * CHANNEL_COUNT)
+        );
     }
     asm volatile(".intel_syntax noprefix");
 	asm volatile("push 231"); //exit_group
