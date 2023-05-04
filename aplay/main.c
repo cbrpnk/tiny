fb59bf0cf53d419ae1af8e8d7edaab58e6a6ae6f
cbrpnk <emmanuelturpin@gmail.com>
2023-05-03 22:49:30 -0400

    pad

--- aplay/main.c
+++ aplay/main.c
@@ -53,13 +53,14 @@ float noise() {
 
 float drum(float time) {
     float sample = osc(time, 40, env(time, 300)*100);
-    sample += noise() * env(time, 30.0f) * .3;
+    sample += noise() * env(time, 80.0f) * .3;
     sample += osc(time, 40, 0);
-    return sample * env(time, 30.0f);
+    return sample * env(time, 50.0f);
 }
 
 float last = 0.;
 float do_bass(float time) {
+    time += tan_h(((m_sin(time*16)*.5+.5)*.03))*.2 + 1;
     float t = time;
     float f = 200 * (int)(modulo(time*2+(int)(m_sin(time)), 2)) * m_sin(t*.1);
     //time = modulo(time, MAX(.8, m_sin(t)*2.));
@@ -72,12 +73,31 @@ float do_bass(float time) {
     return last;
 }
 
-float do_lead(float time) {
-    time += tan_h(((m_sin(time*32)*.25+1)*.1))*.1 + 1;
-    float o1 = osc(time, 1000, 0);
-    float o2 = osc(time, 3000, o1*20);
-    float o3 = osc(time, 4000, o2);
-    return (o1*.2 + o2*.2 + o3*.2) * env(modulo(time, .16), 25);
+float do_lead_voice(float time, float f, float *l, float *r) {
+    float o1 = osc(time, f, 0);
+    float o2 = osc(time, f*1.01, 0);
+    float o3 = osc(time, f*1.013, 0);
+    float o4 = osc(time, f*2.027, 0);
+    float adsr = env(modulo(time, .16), 1);
+    float am = MAX(0, sin(time*128*sin(time)));
+    *l += (o1*.1 + o2*.1 + o3*.3 + o4*.3) * adsr * .2 ;
+    *r += (o1*.3 + o2*.3 + o3*.1 + o4*.1) * adsr * .15;
+}
+
+void do_lead(float time, float *l, float *r) {
+    if(time < 2) {
+        do_lead_voice(time, 200, l,  r);
+        do_lead_voice(time, 300, l,  r);
+        do_lead_voice(time, 400, l,  r);
+    } else if(time < 6) {
+        do_lead_voice(time, 200, l,  r);
+        do_lead_voice(time, 400, l,  r);
+        do_lead_voice(time, 500, l,  r);
+    } else {
+        do_lead_voice(time, 200, l,  r);
+        do_lead_voice(time, 500, l,  r);
+        do_lead_voice(time, 700, l,  r);
+    }
 }
 
 float do_clap(float time) {
@@ -86,26 +106,40 @@ float do_clap(float time) {
 
 float do_hihat(float time) {
     time = modulo(time+.25, .5);
-    return noise() * env(time, 50);
+    return noise() * env(time, 30);
 }
 
 void play(float time, float *left, float *right) {
-    time += tan_h(((m_sin(time*16)*.5+.5)*.03))*.2 + 1;
+    float saved_time = time;
     time = modulo(time, 8)+1;
     float kick = drum(modulo(time, .5));
-    float bass = do_bass(time) * .25;
     
+    float sidechain = (1-env(modulo(time, .5), 30));
+    float bass_l = do_bass(time) * .45 * sidechain;
+    float bass_r = do_bass(time+.001) * .45 * sidechain;
     
-    float lead = do_lead(time) * .25;
     float clap = do_clap(time);
     float hihat = do_hihat(time);
     
     float intro = time > 8;
     
-    float mix = (kick + bass + hihat + lead);
     
-    *left = mix;
-    *right = mix;
+    
+    do_lead(time, left, right);
+    
+        float mix = (kick);
+        *left += mix + bass_l;
+        *right += mix + bass_r;
+    /*
+    if(saved_time > 16) {
+        float mix = (kick + bass);
+        if(saved_time > 32) {
+            mix += hihat;
+        }
+        *left += mix;
+        *right += mix;
+    }
+    */
 }
 
 void _start () {
@@ -116,6 +150,8 @@ void _start () {
     while(1) {
         for(int i=0; i<BUFFER_SIZE*CHANNEL_COUNT; i+=2) {
             float time = (float) frame_id / 44100;
+            buffer[i] = 0.;
+            buffer[i+1] = 0.;
             play(time, buffer+i, buffer+i+1);
             frame_id++;
         }
